6779a8ccd9dd8dfb7d0338564686a2874febf288
tilleryj <tilleryj@gmail.com>
2011-07-10 20:22:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chess::Application.config.secret_token = '8bb05b1aae12e60ccb17f9d22385f5801681aa0c77c983782daf331125b40261ab55f3745932e547f0160d5e35cceb37d39bb748c4372e1fe08254e641ff3a0d'
