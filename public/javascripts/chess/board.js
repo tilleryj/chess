4de5a5c3da1da75a423f72e15ce49f7bb4cd69a8
tilleryj <tilleryj@gmail.com>
2011-07-12 00:09:05 -0500

    Added game logic

--- public/javascripts/chess/board.js
+++ public/javascripts/chess/board.js
@@ -3,26 +3,18 @@ if (!window.Chess) { window.Chess = {}; }
 Chess.Board = {
 	
 	pieceSymbols: {
-		black: {
-			king:   "\u2654",
-			queen:  "\u2655",
-			rook:   "\u2656",
-			bishop: "\u2657",
-			knight: "\u2658",
-			pawn:   "\u2659"
-		},
-
-		white: {
-			king:   "\u265A",
-			queen:  "\u265B",
-			rook:   "\u265C",
-			bishop: "\u265D",
-			knight: "\u265E",
-			pawn:   "\u265F"
-		}
+		k: "\u265A",
+		q: "\u265B",
+		r: "\u265C",
+		b: "\u265D",
+		n: "\u265E",
+		p: "\u265F"
 	},
 	
-	create: function(wrapper) {
+	create: function(options) {
+		
+		var wrapper = options.elt;
+		var game = options.game;
 
 		function createCanvas() {
 			var canvas = $('<canvas width="800" height="800" />');
@@ -33,13 +25,17 @@ Chess.Board = {
 		function draw(processing) {
 			var GRID_SIZE = 100;			
 			
-			function drawPiece(symbol, x, y) {
-				var pieceLeft = x - 28;
+			function drawPiece(symbol, color, x, y) {
+				var pieceLeft = x - 28	;
 				var pieceTop = y + 15;
 
 				processing.createFont("Arial", 60);
 
-				processing.fill(0,0,0);
+				if (color == "w") {
+					processing.fill(0,0,0);
+				} else {
+					processing.fill(255,255,255);
+				}
 				processing.text(symbol, pieceLeft - 1, pieceTop - 1);
 				processing.text(symbol, pieceLeft, pieceTop - 1);
 				processing.text(symbol, pieceLeft + 1, pieceTop - 1);
@@ -49,7 +45,11 @@ Chess.Board = {
 				processing.text(symbol, pieceLeft - 1, pieceTop + 1);
 				processing.text(symbol, pieceLeft - 1, pieceTop);
 
-				processing.fill(255,255,255);
+				if (color == "b") {
+					processing.fill(0,0,0);
+				} else {
+					processing.fill(255,255,255);
+				}
 				processing.text(symbol, pieceLeft, pieceTop);
 			}
 			
@@ -59,7 +59,9 @@ Chess.Board = {
 				processing.size(800, 800);
 				processing.background(50);
 				
-				// Columns, A-H
+				var fen = game.fen();
+				
+				// Columns, h-a
 				for (var column=0; column<=7; column++) {
 					// Rows, 1-8
 					for (var row=0; row<=7; row++) {
@@ -72,9 +74,14 @@ Chess.Board = {
 						var left = column * GRID_SIZE;
 						processing.rect(left, top, GRID_SIZE, GRID_SIZE);
 						
-						var symbol = Chess.Board.pieceSymbols.white.bishop;
+						var square = "hgfedcba"[column] + "87654321"[row];
+						var piece = game.get(square);
+						if (piece) {
+							window.p = piece
+							var symbol = Chess.Board.pieceSymbols[piece.type];
 
-						drawPiece(symbol, left + 50, top + 50);
+							drawPiece(symbol, piece.color, left + 50, top + 50);
+						}
 					}
 				}
 			};
