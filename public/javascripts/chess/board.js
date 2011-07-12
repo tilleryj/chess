5aff6027f9a5546f68b10ee349eb800381908269
tilleryj <tilleryj@gmail.com>
2011-07-12 01:52:34 -0500

    Connected board to game. Made playable with 2 humans

--- public/javascripts/chess/board.js
+++ public/javascripts/chess/board.js
@@ -3,6 +3,12 @@ if (!window.Chess) { window.Chess = {}; }
 Chess.Board = {
 	
 	pieceSymbols: {
+		K: "\u2654",
+		Q: "\u2655",
+		R: "\u2656",
+		B: "\u2657",
+		N: "\u2658",
+		P: "\u2659",
 		k: "\u265A",
 		q: "\u265B",
 		r: "\u265C",
@@ -13,81 +19,57 @@ Chess.Board = {
 	
 	create: function(options) {
 		
-		var wrapper = options.elt;
-		var game = options.game;
+		var wrapper = options.elt,
+			game = options.game,
+			board;
 
-		function createCanvas() {
-			var canvas = $('<canvas width="800" height="800" />');
-			canvas.appendTo(wrapper);
-			return canvas[0];
+		function createBoard() {
+			board = $("<table class='board'></table>");
+			board.appendTo(wrapper);
+
+			for (var row=0; row<8; row++) {
+				$("<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>").appendTo(board);
+			}
 		}
 		
-		function draw(processing) {
-			var GRID_SIZE = 100;			
-			
-			function drawPiece(symbol, color, x, y) {
-				var pieceLeft = x - 28	;
-				var pieceTop = y + 15;
+		function squareFromCell(td) {
+			td = $(td);
+			var column = "abcdefgh"[td.index()];
+			var row = "87654321"[td.closest("tr").index()];
+			return column + row;
+		}
 
-				processing.createFont("Arial", 60);
-
-				if (color == "w") {
-					processing.fill(0,0,0);
-				} else {
-					processing.fill(255,255,255);
-				}
-				processing.text(symbol, pieceLeft - 1, pieceTop - 1);
-				processing.text(symbol, pieceLeft, pieceTop - 1);
-				processing.text(symbol, pieceLeft + 1, pieceTop - 1);
-				processing.text(symbol, pieceLeft + 1, pieceTop);
-				processing.text(symbol, pieceLeft + 1, pieceTop + 1);
-				processing.text(symbol, pieceLeft, pieceTop + 1);
-				processing.text(symbol, pieceLeft - 1, pieceTop + 1);
-				processing.text(symbol, pieceLeft - 1, pieceTop);
-
-				if (color == "b") {
-					processing.fill(0,0,0);
-				} else {
-					processing.fill(255,255,255);
-				}
-				processing.text(symbol, pieceLeft, pieceTop);
-			}
-			
-			
-			processing.draw = function() {
-				
-				processing.size(800, 800);
-				processing.background(50);
-				
-				var fen = game.fen();
-				
-				// Columns, h-a
-				for (var column=0; column<=7; column++) {
-					// Rows, 1-8
-					for (var row=0; row<=7; row++) {
-						if ((row + column) % 2 == 0) {
-							processing.fill(255,255,255);
-						} else {
-							processing.fill(0,0,0);
-						}
-						var top = row * GRID_SIZE;
-						var left = column * GRID_SIZE;
-						processing.rect(left, top, GRID_SIZE, GRID_SIZE);
-						
-						var square = "hgfedcba"[column] + "87654321"[row];
-						var piece = game.get(square);
-						if (piece) {
-							window.p = piece
-							var symbol = Chess.Board.pieceSymbols[piece.type];
-
-							drawPiece(symbol, piece.color, left + 50, top + 50);
-						}
+		function draw() {
+			var fen = game.fen();
+			$("td", board).text("");
+			// Columns, h-a
+			for (var column=0; column<=7; column++) {
+				// Rows, 1-8
+				for (var row=0; row<=7; row++) {
+					var square = "abcdefgh"[column] + "87654321"[row];
+					var piece = game.get(square);
+					if (piece) {
+						var symbol = Chess.Board.pieceSymbols[piece.type[piece.color == 'w' ? 'toUpperCase' : 'toLowerCase']()];
+						$($("td", board)[row * 8 + column]).html("<span data-square='" + square + "'>" + symbol + "</span>");
 					}
 				}
-			};
+			}
+			
+			$("td span", board).draggable({ revert: true, revertDuration: 200 });
+			$("td").droppable({
+				hoverClass: "pieceHover",
+				drop: function(event, ui) {
+					game.move({
+						from: ui.draggable.data("square"),
+						to: squareFromCell($(this)[0])
+					});
+					draw();
+				}
+			});
 		}
-		
-		processing = new Processing(createCanvas(), draw);
+
+		createBoard();
+		draw();
 	}
 };
 
